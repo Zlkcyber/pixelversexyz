bb2ff65c251dfa6089496544847cc3cefd397a43
Lord_dubious <chukwurahdavid@gmail.com>
2024-05-26 20:19:38 +0100

    Update main.py
    fixes 
    User
    SyntaxError 'username': username

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ headers = {
     'sec-fetch-site': 'cross-site',
     'secret': secret,
     'tg-id': tgId,
-    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
     'username': username
 }
 
